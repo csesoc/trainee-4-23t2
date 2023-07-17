fd98cb60f0b8923ee025b33f3b2a015c3383c35c
CHUN Nicole Wai Yan <19chunnw3@Nicole-Chun-MacBookPro.local>
2023-07-17 17:19:38 +1000

    added colours to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'theme-red': '#992346',
+        'theme-blue': '#645CA7',
+        'theme-black': '#322C36',
+        'theme-pink': '#E48DC7',
+        'theme-white': '#EBC7E7',
+      }
+    },
   },
   plugins: [],
 }
