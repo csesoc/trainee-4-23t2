2fa20e50209646323007a981fad2dbc3c1d04635
Jayden Nguyen <jayden.m.nguyen@student.unsw.edu.au>
2023-08-26 14:35:58 +1000

    finished and properly fixed css things

--- src/pages/PracticeModePage/PracticePage.tsx
+++ src/pages/PracticeModePage/PracticePage.tsx
@@ -1,14 +1,21 @@
 import './PracticePage.css';
-import '../../components/PracticeQuestion'
+import '../../components/css/PracticeQuestionAnswer.css'
+import '../../components/PracticeQuestionAnswer'
 import { ExitPage } from "../../components/ExitButton";
-import PracticeQuestion from '../../components/PracticeQuestion';
+import PracticeQuestion from '../../components/PracticeQuestionAnswer';
+import { MdOutlineBookmark } from 'react-icons/md'
 
 export function PracticePage() {
   return (
     <>
       <ExitPage/>
-      <div style={{display:'flex', justifyContent: 'center', alignItems: 'center', height: '80vh'}}>
+      <div id='question-answer-container' style={{display:'flex', width: '80%', height: '85vh', boxSizing: 'border-box', border: "1px solid red", flexWrap: "wrap", margin: 'auto'}}>
         <PracticeQuestion/>
+        <div id="button-container">
+          <button id="save-question" className="next-save"> <MdOutlineBookmark style={{display: "inline-block"}}/> save question </button>
+          <button id="next-question" className="next-save"> next question </button>
+        </div>
+
       </div>
     </>
   )
