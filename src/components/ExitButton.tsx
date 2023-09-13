9d121c27aca241261989e4db38cb0e0db65842a6
Jayden Nguyen <jayden.m.nguyen@student.unsw.edu.au>
2023-09-13 19:15:14 +1000

    added a clearQData func

--- src/components/ExitButton.tsx
+++ src/components/ExitButton.tsx
@@ -1,9 +1,15 @@
 import { useNavigate } from "react-router-dom";
 import { IoCaretBack } from 'react-icons/io5';
 
+function clearQData() {
+    localStorage.removeItem('qData');
+    localStorage.removeItem('answered');
+    localStorage.removeItem('qCount');
+}
+
 export function ExitPage() {
     const navigate = useNavigate();
     return (
-        <button onClick={() => { localStorage.clear(); navigate('/');}} className="top-left" style={{position:"absolute", top:"5%", left:"2.5%", fontSize:"30px"}}> <IoCaretBack style={{display: "inline-block"}}/>Exit</button>
+        <button onClick={() => { clearQData(); navigate('/');}} className="top-left" style={{position:"absolute", top:"5%", left:"2.5%", fontSize:"30px"}}> <IoCaretBack style={{display: "inline-block"}}/>Exit</button>
     );
 }
